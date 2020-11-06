ae3f6b695e44adf503670425cc21776f894f4e5f
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2020-11-06 14:18:31 +0100

    Update for Sensible Instagram

--- Sensible Instagram.user.js
+++ Sensible Instagram.user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name      Sensible Instagram
-// @version   1
+// @version   20201106
 // @grant     none
 // @namespace https://www.instagram.com
-// @include   https://www.instagram.com
+// @include   https://www.instagram.com/*
 // ==/UserScript==
 
 window.addEventListener('load', function() {
 	document.getElementsByTagName('nav')[0].style.display = 'none';  
 }, false);
 
-var target = document.querySelector('body');
-observer = new MutationObserver(function(mutation) {
+let target = document.querySelector('body');
+let observer = new MutationObserver(function(mutation) {
 	document.getElementsByTagName('body')[0].style.overflow = 'auto';
-  document.getElementsByClassName('_Yhr4')[0].style.display = 'none';
+    document.getElementsByClassName('RnEpo')[0].style.display = 'none';
 });
 
-config = {
+let config = {
     attributes: true
 };
 
